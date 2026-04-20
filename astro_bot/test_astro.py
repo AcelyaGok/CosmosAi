a0144216e80772fa02607370274cf6f9422d68f7
Açelya Gök <acelyagokk@gmail.com>
2026-04-20 14:36:51 +0300

    Sprint 1 tamamlandı

---
+++ astro_bot/test_astro.py
@@ -0,0 +1,12 @@
+import json
+from astro_calculator import calculate_natal_chart
+
+result = calculate_natal_chart(
+    year=1995, month=6, day=15,
+    hour=14, minute=30,
+    latitude=41.0082,
+    longitude=28.9784,
+    utc_offset=3.0
+)
+
+print(json.dumps(result, ensure_ascii=False, indent=2))
